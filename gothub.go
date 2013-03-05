f5bb3af98f29f69da0be43a5cb9478eed59804d3
Nick Saika <nicksaika@gmail.com>
2013-03-05 00:26:10 -0500

    Fixed the "Authorization" header; it was returning a 401 error.

--- gothub.go
+++ gothub.go
@@ -12,16 +12,20 @@ const GitHubUrl string = "https://api.github.com"
 
 // The GitHub struct represents an active session to the GitHub API.
 type GitHub struct {
-	httpClient &http.Client
-	authorization string
+	httpClient *http.Client
+	Authorization string
+}
+
+func hashAuth(u, p string) string {
+	var a = fmt.Sprintf("%s:%s", u, p)
+	return base64.StdEncoding.EncodeToString([]byte(a))
 }
 
 // Log in to GitHub using basic, username/password authentication.
 func BasicLogin(username, password string) (*GitHub, error) {
 	// Format and Base64-encode the provided username and password, in preparation for basic
 	// HTTP auth.
-	basic := fmt.Sprintf("%s:%s", username, password)
-	authorization := base64.StdEncoding.EncodeToString([]byte(basic))
+	authorization := fmt.Sprintf("Basic %s", hashAuth(username, password))
 	request, err := http.NewRequest("GET", GitHubUrl, nil)
 	if err != nil {
 		return nil, err
@@ -33,14 +37,14 @@ func BasicLogin(username, password string) (*GitHub, error) {
 	// Create a new HTTP client (which we will eventually provide to the GitHub struct), for
 	// issuing the above HTTP request, and future requests.
 	client := &http.Client{}
-	response, err := client.Do(&request)
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
 
 	if response.StatusCode == http.StatusOK {
 		// Yaaaaaaay!
-		return &GitHub{httpClient: client, authorization: authorization}, nil
+		return &GitHub{httpClient: client, Authorization: authorization}, nil
 	}
 
 	// Should we get here, the basic authentication request failed.

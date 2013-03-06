1ac868b2e0fec875787b5253efe7c0bddc3d01f9
Nick Saika <nicksaika@gmail.com>
2013-03-06 18:26:22 -0500

    Got back to a good, working state where tests pass

--- users.go
+++ users.go
@@ -2,7 +2,6 @@ package gothub
 
 import (
 	"fmt"
-	"net/url"
 	"time"
 )
 
@@ -12,25 +11,29 @@ import (
 // Please take note that you cannot use the User struct to modify the details of a user's account.
 // To do this, please look at the CurrentUser struct.
 type User struct {
-	Login       string `json:"login"`
-	Id          int    `json:"id"`
-	AvatarUrl   url.URL
-	GravatarId  string `json:"gravatar_id"`
-	Url         url.URL
-	Name        string `json:"name"`
-	Company     string `json:"company"`
-	Blog        url.URL
-	Location    string `json:"location"`
-	Email       string `json:"email"`
-	Hireable    bool   `json:"hireable"`
-	Bio         string `json:"bio"`
-	PublicRepos int    `json:"public_repos"`
-	PublicGists int    `json:"public_gists"`
-	Followers   int    `json:"followers"`
-	Following   int    `json:"following"`
-	HtmlUrl     url.URL
-	CreatedAt   time.Time
-	Type        string `json:"type"`
+	Login       string    `json:"login"`
+	Id          int       `json:"id"`
+	AvatarUrl   string    `json:"avatar_url"`
+	GravatarId  string    `json:"gravatar_id"`
+	Url         string    `json:"url"`
+	Name        string    `json:"name"`
+	Company     string    `json:"company"`
+	Blog        string    `json:"blog"`
+	Location    string    `json:"location"`
+	Email       string    `json:"email"`
+	Hireable    bool      `json:"hireable"`
+	Bio         string    `json:"bio"`
+	PublicRepos int       `json:"public_repos"`
+	PublicGists int       `json:"public_gists"`
+	Followers   int       `json:"followers"`
+	Following   int       `json:"following"`
+	HtmlUrl     string    `json:"html_url"`
+	CreatedAt   time.Time `json:"created_at"`
+	Type        string    `json:"type"`
+}
+
+func (u *User) Emails() ([]string, error) {
+	return nil, nil
 }
 
 // Returns the details of a single user, as specified by their "login".
@@ -40,22 +43,21 @@ type User struct {
 //    https://github.com/<login>
 func (g *GitHub) GetUser(login string) (*User, error) {
 	var user User
-	if err := g.callGithubApi("GET", fmt.Sprintf("/users/%s", login), &user); err != nil {
+	err := g.callGithubApi("GET", fmt.Sprintf("/users/%s", login), &user)
+	if err != nil {
 		return nil, err
 	}
-	return &user, nil
-}
 
-// Get a list of every single user on GitHub.
-func GetAllUsers() ([]*User, error) {
-	return nil, nil
+	return &user, nil
 }
 
 // Returns the currently-authenticated user, as a pointer to a User struct.
 func (g *GitHub) GetCurrentUser() (*User, error) {
 	var user User
-	if err := g.callGithubApi("GET", "/user", &user); err != nil {
+	err := g.callGithubApi("GET", "/user", &user)
+	if err != nil {
 		return nil, err
 	}
+
 	return &user, nil
 }
--- users_test.go
+++ users_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
+func TestGetUser(t *testing.T) {
+	u, p, err := getTestingCredentials()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var g *GitHub
+	g, err = BasicLogin(u, p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if user, err := g.GetUser(u); err != nil {
+		t.Error(err)
+	} else {
+		t.Logf("ID: %d", user.Id)
+		t.Logf("Login: %s", user.Login)
+	}
+}
+
 func TestGetCurrentUser(t *testing.T) {
 	username, password, err := getTestingCredentials()
 	if err != nil {
@@ -17,27 +35,9 @@ func TestGetCurrentUser(t *testing.T) {
 	}
 
 	t.Logf("Authorization: %s", g.Authorization)
-	
+
 	user, err := g.GetCurrentUser()
 	if err != nil {
-		t.Error(err)
-	} else {
-		t.Logf("ID: %d", user.Id)
-		t.Logf("Login: %s", user.Login)
-	}
-}
-
-func TestGetUser(t *testing.T) {
-	u, p, err := getTestingCredentials()
-	if err != nil {
-		t.Fatal(err)
-	}
-	var g *GitHub
-	g, err = BasicLogin(u, p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if user, err := g.GetUser(u); err != nil {
 		t.Error(err)
 	} else {
 		t.Logf("ID: %d", user.Id)

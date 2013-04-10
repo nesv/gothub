098b33d7a638e9504b31235e3ca23901939e4f90
Nick Saika <nicksaika@gmail.com>
2013-04-10 18:55:34 -0400

    `go fmt`

--- users.go
+++ users.go
@@ -40,22 +40,22 @@ type User struct {
 
 // A minimized form of the User struct, for holding information about a follower.
 type Follower struct {
-	Login      string `json:"login"`
-	Id         int    `json:"id"`
-	AvatarUrl  string `json:"avatar_url"`
-	GravatarId string `json:"gravatar_id"`
-	Url        string `json:"url"`
-	HtmlUrl string `json:"html_url"`
-	FollowersUrl string `json:"followers_url"`
-	FollowingUrl string `json:"following_url"`
-	GistsUrl string `json:"gists_url"`
-	StarredUrl string `json:"starred_url"`
-	SubscriptionsUrl string `json:"subscriptions_url"`
-	OrganizationsUrl string `json:"organizations_url"`
-	ReposUrl string `json:"repos_url"`
-	EventsUrl string `json:"events_url"`
+	Login             string `json:"login"`
+	Id                int    `json:"id"`
+	AvatarUrl         string `json:"avatar_url"`
+	GravatarId        string `json:"gravatar_id"`
+	Url               string `json:"url"`
+	HtmlUrl           string `json:"html_url"`
+	FollowersUrl      string `json:"followers_url"`
+	FollowingUrl      string `json:"following_url"`
+	GistsUrl          string `json:"gists_url"`
+	StarredUrl        string `json:"starred_url"`
+	SubscriptionsUrl  string `json:"subscriptions_url"`
+	OrganizationsUrl  string `json:"organizations_url"`
+	ReposUrl          string `json:"repos_url"`
+	EventsUrl         string `json:"events_url"`
 	ReceivedEventsUrl string `json:"received_events_url"`
-	Type string `json:"type"`
+	Type              string `json:"type"`
 }
 
 // Gets a detailed list of a user's followers.
